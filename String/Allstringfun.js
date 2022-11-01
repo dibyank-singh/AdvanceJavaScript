ca6abecff8fad93751cbf7f4dbf1359cf1b4dfce
Dibyank kumar singh <76806222+dibyank-singh@users.noreply.github.com>
2022-11-01 20:34:40 +0530

    String Methods added

---
+++ String/Allstringfun.js
@@ -0,0 +1,23 @@
+// All Useful Sting Methods.
+
+const name="Divyank"
+
+// console.log( name.length )    => 7
+// console.log(name.charAt(5) )    
+//   => n
+console.log( name.concat("" + " Singh") )    
+// Divyank Singh
+console.log( name.indexOf("v") )    
+// => 2
+console.log( name.repeat(2) )    
+// => DivyankDivyank
+console.log( name.search("a"))    
+// => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
+// console.log( name.length )    => 7
